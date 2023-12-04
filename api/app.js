c7b193ea3ae84d549376436e0ffdc43a29f540c7
Sergisley <sergisley@gmail.com>
2023-12-04 18:18:55 -0300

    adição do sequelize e migration ajustes integração frontend

--- api/app.js
+++ api/app.js
@@ -3,10 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require("cors");
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var testAPIRouter = require('./routes/testAPI');
+var testsRouter = require('./routes/tests');
+var projectsRouter = require('./routes/projects');
+var tasksRouter = require('./routes/tasks');
 
 var app = express();
 
@@ -14,6 +18,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,8 +26,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/tests', testsRouter);
 app.use('/users', usersRouter);
-app.use('/testAPI', testAPIRouter);
+app.use('/tasks', tasksRouter);
+app.use('/projects', projectsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ api/routes/tests.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.send({"message":"The API is working correctly"});
+});
+
+module.exports = router;
